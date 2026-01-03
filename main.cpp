d51c2897c3d1a12f97b2158c2c0808145c948daa
Lazy Dev <groketacess30@gmail.com>
2026-01-03 18:55:32 +0100

    Made it so ID fields are auto incremented and percistant in records

--- main.cpp
+++ main.cpp
@@ -11,8 +11,13 @@ namespace fs = std::filesystem;
 
 struct DataBankHeader {
     uint32_t MAGIC;
-    uint32_t VERSION; 
-    //DataBankHeader(uint32_t m, uint32_t v) : MAGIC(m), VERSION(v) {}
+    uint32_t VERSION;
+    uint32_t ID_INDEX = 0;
+};
+struct IndexHeader {
+    uint32_t MAGIC;
+    uint32_t VERSION;
+    uint8_t DATATYPE;
 };
 
 enum class DataType : uint8_t {
@@ -31,9 +36,10 @@ struct Row {
 class Table {
     private:
         DataBankHeader DBheader{0x44415441, 1};
+        
         template <typename T>
         bool save_header(ostream& out, const T& header) {
-           out.write(reinterpret_cast<const char*>(&header), sizeof(T));
+            out.write(reinterpret_cast<const char*>(&header), sizeof(T));
             return out.good();
         }
         
@@ -51,26 +57,55 @@ class Table {
             return true;
         }
 
-        bool append(std::ostream& out, const Row& row) {
-            uint32_t row_count = static_cast<uint32_t>(row.values.size());
+        bool append(std::fstream& out, const Row& row) {
+            
+            // 2 is the index of id index offcet in the header struct.
+            // 4 is the size of each value in the header struct, each are 4 bytes wide.
+            int id_index_offset = 2 * 4;
+            uint32_t index = 0;
+            out.seekg(id_index_offset, ios::beg);
+            out.read(reinterpret_cast<char*>(&index), sizeof(index));
+            //reset read cursor:
+            out.seekg(0, ios::beg);
+
+            index += 1;
+            //Move the write cursor to the end of the file to append:
+            out.seekp(0, ios::end);
+
+            uint32_t row_count = row.values.size() + 1;
             out.write(reinterpret_cast<const char*>(&row_count), sizeof(row_count));
-        
+
+            uint8_t id_type = 1;
+            out.write(reinterpret_cast<const char*>(&id_type), sizeof(id_type));
+            out.write(reinterpret_cast<const char*>(&index), sizeof(index));
+
             for (const auto& value : row.values) {
                 std::visit([&out](const auto& payload) {
                     using T = std::decay_t<decltype(payload)>;
-                    if constexpr (std::is_same_v<T, int32_t>) {
-                        uint8_t type = 1;
-                        out.write(reinterpret_cast<const char*>(&type), sizeof(type));
-                        out.write(reinterpret_cast<const char*>(&payload), sizeof(payload));  // now 4 bytes
-                    } else if constexpr (std::is_same_v<T, std::string>) {
-                        uint8_t type = 2;
-                        out.write(reinterpret_cast<const char*>(&type), sizeof(type));
-                        uint32_t len = static_cast<uint32_t>(payload.size());
-                        out.write(reinterpret_cast<const char*>(&len), sizeof(len));
-                        out.write(payload.data(), len);
-                    }
+                        if constexpr (std::is_same_v<T, int32_t>) {
+
+                            uint8_t type = 1;
+                            out.write(reinterpret_cast<const char*>(&type), sizeof(type));
+                            out.write(reinterpret_cast<const char*>(&payload), sizeof(payload));  // now 4 bytes
+                            
+
+                        } else if constexpr (std::is_same_v<T, std::string>) {
+
+                            uint8_t type = 2;
+                            out.write(reinterpret_cast<const char*>(&type), sizeof(type));
+                            uint32_t len = static_cast<uint32_t>(payload.size());
+                            out.write(reinterpret_cast<const char*>(&len), sizeof(len));
+                            out.write(payload.data(), len);
+                    
+
+                        }
                 }, value);
             }
+
+            //Update index counter:
+            out.seekp(id_index_offset, ios::beg);
+            out.write(reinterpret_cast<const char*>(&index), sizeof(index));
+            out.seekp(0, ios::beg);
             return out.good();
         }
 
@@ -123,10 +158,13 @@ class Table {
 
         fs::path folder_path;
         fs::path databank_path;
+        fs::path index_path;
 
-        Table(string n) : name(n){
+        Table(string n, const vector<Column>& s) : name(n), schema(s){
+
             folder_path = fs::path(this->name);
             fs::create_directories(folder_path);
+            
             databank_path = fs::path(folder_path) / "DataBank.bin";
             if(!fs::exists(databank_path)) {
                 ofstream ofs(databank_path, ios::binary);
@@ -134,14 +172,25 @@ class Table {
                     cout << "Failed to initiate DataBank." << endl;
                 }
             }
+
+            index_path = fs::path(folder_path) / "index";
+            if(!fs::exists(index_path)) {
+                fs::create_directories(index_path);
+                ofstream(fs::path(index_path) / "index-id.idx").close();
+                for(auto& column: this->schema) {
+                    string file_name = "index-" + column.name + ".idx";
+                    ofstream(fs::path(index_path) / file_name, ios::binary);
+                }
+            }
+
             cout << "Table: " << this->name << " Created." << endl;
         }
-        
+
         void appendRow(const Row& row) {
             
-            ofstream out(this->databank_path, ios::app | ios::binary);
+            fstream out(this->databank_path, ios::binary | ios::in | ios::out);
             if (!out) {
-                cout << "Failed to Open file" << endl;
+                cout << "Failed to Open file when Appending." << endl;
                 return;
             }
             if (!this->append(out, row)) {
@@ -165,6 +214,7 @@ class Table {
                 return;
             }
 
+
             data.clear();  // Reset internal data
             while (in.peek() != EOF) {
                 Row row;
@@ -175,6 +225,14 @@ class Table {
                 }
                 data.push_back(row);
             }
+        }
+
+        void print_id() {
+            fstream out(this->databank_path, ios::binary | ios::in | ios::out);
+            out.seekg(2 * 4, ios::beg);
+            uint32_t index = 0;
+            out.read(reinterpret_cast<char*>(&index), sizeof(index));
+            cout << "Index: " << index << endl;
         }
 
 };
@@ -192,20 +250,23 @@ int main() {
         { {"Dude", 16} }
     };
     
-    Table table("Dudes");
+    Table table("Dudes", columns);
 
-    //Row add;
-    //add.values.push_back("Pippa");
-    //add.values.push_back(100);
-    ////add2.values.push_back("Dude");
-    //add2.values.push_back(50);
-    //Row add3;
-    //add2.values.push_back("Yohan the Butcher");
-    //add2.values.push_back(25);
+    Row add;
+    add.values.push_back("Pippa");
+    add.values.push_back(100);
+    Row add2;
+    add2.values.push_back("Dude");
+    add2.values.push_back(50);
+    Row add3;
+    add2.values.push_back("Yohan the Butcher");
+    add2.values.push_back(25);
     Row add4;
     add4.values.push_back("Kirche");
     add4.values.push_back(99);
-    vector<Row> rows2 = {add4};
+    add4.values.push_back("Fox God");
+
+    vector<Row> rows2 = { {add, add2, add3, add4} };
 
     for (auto i: rows2) {
         table.appendRow(i);
@@ -216,23 +277,25 @@ int main() {
         cout << "Row " << row_num++ << ":" << endl;
         int count = 1;
         for (auto item: row.values) {
-            cout << "item number: " << count << endl;
+            //cout << "item number: " << count << endl;
             count ++;
             visit([](const auto& x) {
                 using T = std::decay_t<decltype(x)>;
     
                 if constexpr (std::is_same_v<T, int32_t>) {
-                    cout << "Type: int" << endl;
+                    //cout << "Type: int" << endl;
                     cout << "|| " << x << " ||" << endl;
                 }
                 else if constexpr (std::is_same_v<T, std::string>) {
-                    cout << "Type: str" << endl;
+                    //cout << "Type: str" << endl;
                     cout << "|| " << x << " ||" << endl;
                 }
             }, item);
         }
         cout << "SIZE: " << row.values.size() << endl;
     }
+
+    table.print_id();
 
     return 0;
 }
